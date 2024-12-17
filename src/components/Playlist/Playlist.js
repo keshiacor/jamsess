7e0de6be8031eb0121e2c4b3e9530c70e6d932ba
kcfit <keshiacorioll@me.com>
2024-12-17 15:57:00 +0100

    adding search and save buttons

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,2 +1,13 @@
 import React, { useState, useCallback } from "react";
 import "./Playlist.css";
+
+const Playlist = (props) => {
+
+return (
+  <div>
+    <button>
+    Save To Spotify
+    </button>
+  </div>
+)
+}
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,13 @@
 import React, { useState, useCallback } from "react";
 import "./SearchBar.css";
+
+const SearchBar = (props) => {
+
+  return (
+    <div>
+      <button>
+       Search
+      </button>
+    </div>
+  )
+}
